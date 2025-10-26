fe6dce42481466992c0f456950c47076494415a3
farzanatavakkoli <t.farzanatavakoli.3@gmail.com>
2025-10-26 17:50:45 +0430

    Added basic javascript

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,9 @@
+function searchHandle(event) {
+  event.preventDefault();
+  let searchElement = document.querySelector("#search-form-input");
+  let city = document.querySelector("#city");
+  city.innerHTML = searchElement.value;
+}
 
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", searchHandle);
