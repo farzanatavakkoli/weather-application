b1f2c48b052c75ea2b1124ca93fc1761b2beafec
farzanatavakkoli <t.farzanatavakoli.3@gmail.com>
2025-10-26 23:09:40 +0430

    Added search engine

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,25 @@
+function updateCurrentWeather(response) {
+  let currentTemp = document.querySelector("#current-temperature-value");
+  currentTemp.innerHTML = Math.round(response.data.temperature.current);
+  let city = document.querySelector("#city");
+  city.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  apiKey = "25db17t7533c78o4da26332a44f6038e";
+  apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(updateCurrentWeather);
+}
+
 function searchHandle(event) {
   event.preventDefault();
   let searchElement = document.querySelector("#search-form-input");
-  let city = document.querySelector("#city");
-  city.innerHTML = searchElement.value;
+
+  searchCity(searchElement.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchHandle);
+
+searchCity("Kabul");
